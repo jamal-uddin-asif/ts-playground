83b3e5056e387f0371494ec41c4fe9e34aceb221
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-09-10 22:36:06 +0600

    Create TCommonCustomerProp and added come practice code

--- sample.ts
+++ sample.ts
@@ -4,19 +4,33 @@ const addCustomerToOrder = <T>(customerInfo: T) => {
 };
 
 const customer1 = { id: 222, name: "Asiq", hasCoupon: true };
+type TCommonCustomerProp = { id: number; name: string; hasCoupon: boolean };
+const result1 = addCustomerToOrder<TCommonCustomerProp>(customer1);
+// console.log(result1);
 
-const result = addCustomerToOrder<{
-  id: number;
-  name: string;
-  hasCoupon: boolean;
-}>(customer1);
-// console.log(result);
+const customer2 = { id: 222, name: "Asiq", hasCoupon: true, moneyBag: 20000 };
+const result2 = addCustomerToOrder<TCommonCustomerProp & { moneyBag: number }>(
+  customer2,
+);
+// console.log(result2);
+// note: <commonCustomerProp & { moneyBag: number }> ekhane Intersection kora hoyeche
 
-const customer2 = { id: 222, name: "Asiq", hasCoupon: true, moneyBag: 20000 }
-const result2 = addCustomerToOrder<{
-  id: number;
-  name: string;
-  hasCoupon: boolean;
-  moneyBag: number;
-}>(customer2);
-console.log(result2);
+//****************** */ Practice ****************************
+const createOrder = <T>(customerInfo: T) => {
+  return { ...customerInfo };
+};
+
+type TOrderProp = { id: number; name: string; hasCoupon: boolean };
+const orderRes1 = createOrder<TOrderProp>({
+  id: 222,
+  name: "Asiq",
+  hasCoupon: true,
+});
+const orderRes2 = createOrder<TOrderProp & { moneyBag: number }>({
+  id: 222,
+  name: "Asiq",
+  hasCoupon: true,
+  moneyBag: 20000,
+});
+ 
+console.log({orderRes1, orderRes2})
