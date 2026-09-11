4e23e8c0e2cbd368c59d3c3c13119c038190f984
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-09-11 12:29:40 +0600

    Try keyof

--- sample.ts
+++ sample.ts
@@ -1,5 +1,8 @@
 // Generic type in function
-const addCustomerToOrder = <T>(customerInfo: T) => {
+// constraint id, name
+const addCustomerToOrder = <T extends { id: number; name: string }>(
+  customerInfo: T,
+) => {
   return { ...customerInfo, orderStatus: "panding" };
 };
 
@@ -12,8 +15,15 @@ const customer2 = { id: 222, name: "Asiq", hasCoupon: true, moneyBag: 20000 };
 const result2 = addCustomerToOrder<TCommonCustomerProp & { moneyBag: number }>(
   customer2,
 );
+const result3 = addCustomerToOrder<{ id: number; name: string; emni: string }>({
+  emni: "emni",
+  id: 222,
+  name: "Asiq",
+});
+
 // console.log(result2);
 // note: <commonCustomerProp & { moneyBag: number }> ekhane Intersection kora hoyeche
+// note2: constraint id,name-> <T extends {id: number, name: string}> ekhane extends {} mane perameter a ar jai asuk {} er bitore ja dibo ta must thakte hobe
 
 //****************** */ Practice ****************************
 const createOrder = <T>(customerInfo: T) => {
@@ -21,16 +31,16 @@ const createOrder = <T>(customerInfo: T) => {
 };
 
 type TOrderProp = { id: number; name: string; hasCoupon: boolean };
-const orderRes1 = createOrder<TOrderProp>({
-  id: 222,
-  name: "Asiq",
-  hasCoupon: true,
-});
-const orderRes2 = createOrder<TOrderProp & { moneyBag: number }>({
-  id: 222,
-  name: "Asiq",
-  hasCoupon: true,
-  moneyBag: 20000,
-});
- 
-console.log({orderRes1, orderRes2})
+const orderRes1 = createOrder<TOrderProp>(customer1);
+const orderRes2 = createOrder<TOrderProp & { moneyBag: number }>(customer2);
+
+console.log({ orderRes1, orderRes2 });
+
+type FoodManu = {
+  burger: string;
+  pizza: string;
+  puchka: string;
+};
+
+const myFavFood1: "burger" | "pizza" | "puchka" = "pizza";
+const myFavFood: keyof FoodManu = "pizza";
